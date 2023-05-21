ba06ad76592f2221c850eac4aafa4a81295cd266
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-05-21 15:38:17 -0700

    add data.txt file to store high score

--- scoreboard.py
+++ scoreboard.py
@@ -13,7 +13,8 @@ class ScoreBoard(Turtle):  # ScoreBoard object is also a Turtle object
         self.speed("fastest")  # so that we wont see scoreboard object moving from (0,0) to the location set in the following line
         self.goto(0, 270)
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as score_file:
+            self.high_score = int(score_file.read())
         self.is_lost = False
         self.update_score_board()
 
@@ -28,6 +29,9 @@ class ScoreBoard(Turtle):  # ScoreBoard object is also a Turtle object
     def game_over(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as score_file:
+                score_file.write(f"{self.high_score}")
+
         self.is_lost = True
         self.update_score_board()
         self.goto(0, 0)
